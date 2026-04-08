1a6b51f6e1ee2cb723dc7a091d4703f1d834cc74
MD. ABDUL HAMIM <124155317+hamim5264@users.noreply.github.com>
2026-04-08 20:38:34 +0600

    added admin models

--- shopify_app/admin.py
+++ shopify_app/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import AccessCode, ShopifyStore
+
+
+@admin.register(AccessCode)
+class AccessCodeAdmin(admin.ModelAdmin):
+    list_display = ("code", "order_id", "email", "email_sent")
+    search_fields = ("email", "code", "order_id")
+    list_filter = ("email_sent",)
+
+
+@admin.register(ShopifyStore)
+class ShopifyStoreAdmin(admin.ModelAdmin):
+    list_display = ("shop_domain", "access_token")
